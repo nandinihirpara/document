4965cb4e8f45ecda803f6d7beca9a3a5fd23c751
nandinihirpara <119646455+nandinihirpara@users.noreply.github.com>
2023-01-10 08:39:40 +0530

    Add files via upload

---
+++ reversSentence.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void reverseSentence()
+{
+    char v;
+    scanf("%c", &v);
+    if (v != '\n')
+    {
+        reverse sentence();
+        printf("%c", v);
+    }
+}
+int main()
+{
+    printf("enter any sentence =");
+    reverse sentence();
+    return 0;
+}
